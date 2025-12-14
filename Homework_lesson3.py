56ab0cbb7dd3974a1ff9df359074cec9c18cfbcf
Iryna Korolko <irina.piatenko@gmail.com>
2025-12-15 00:54:03 +0200

    com2

--- Homework_lesson3.py
+++ Homework_lesson3.py
@@ -1,1 +1,14 @@
-num1 = input ("Введіть перше число:")
+num1 = int(input ("Введіть перше число: "))
+num2 = int(input ("Введіть друге число: "))
+operation = input ("Введіть операцію з числами: ")
+if operation is "+":
+    print (num1 + num2)
+elif operation is "-":
+    print (num1 - num2)
+elif operation is "*":
+    print (num1 * num2)
+elif operation is "/":
+    if num2 == 0:
+        print ("Ділити на нуль не можна")
+    else:
+        print (num1 / num2)
