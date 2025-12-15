9bc6e51addef16b67bb10dc51192acdafada4183
Iryna Korolko <irina.piatenko@gmail.com>
2025-12-15 10:42:59 +0200

    com1

---
+++ Homework_lesson3_t3.py
@@ -0,0 +1,10 @@
+lst = [2, 3, 6, 7, 8]
+length_lst = len(lst)
+if length_lst == 0:
+    print ([], [])
+elif length_lst >= 1:
+    middle = length_lst//2
+    print ([], [])
+else:
+    middle2 = (length_lst//2) +1
+    print ([], [])
