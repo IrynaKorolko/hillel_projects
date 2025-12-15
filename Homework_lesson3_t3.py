a34adbfc7dc929c7146eacc698cfa4be3973a45d
Iryna Korolko <irina.piatenko@gmail.com>
2025-12-15 15:27:24 +0200

    com2

--- Homework_lesson3_t3.py
+++ Homework_lesson3_t3.py
@@ -2,9 +2,10 @@ lst = [2, 3, 6, 7, 8]
 length_lst = len(lst)
 if length_lst == 0:
     print ([], [])
-elif length_lst >= 1:
-    middle = length_lst//2
-    print ([], [])
 else:
-    middle2 = (length_lst//2) +1
-    print ([], [])
+    middle_index = (length_lst + 1) // 2
+    first_list = lst[:middle_index]
+    second_list = lst[middle_index:]
+    result = [first_list, second_list]
+    print(f"Початковий список: {lst}")
+    print(f"Результат: {result}")
