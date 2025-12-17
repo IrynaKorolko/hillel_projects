18d5767c0139c71653d6bfac2b66d90e8789b7c6
Iryna Korolko <irina.piatenko@gmail.com>
2025-12-17 22:54:24 +0200

    com1

--- Homework_lesson4_t3.py
+++ Homework_lesson4_t3.py
@@ -2,4 +2,4 @@ list = [0, 1, 7, 2, 4, 6, 8, 10]
 list_with_indexes = list[0:10:2]
 print(list_with_indexes)
 sum_el_with_indexes = sum(list_with_indexes)
-print(sum_el_with_indexes*list.index(-1))
+print(sum_el_with_indexes*list[-1])
