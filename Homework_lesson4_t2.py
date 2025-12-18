2c05290268ca9aebb07499633b90dc5d702ff050
Iryna Korolko <irina.piatenko@gmail.com>
2025-12-18 13:58:44 +0200

    com4

--- Homework_lesson4_t2.py
+++ Homework_lesson4_t2.py
@@ -1,9 +1,10 @@
 import random
 my_list = []
-for i in range (5):
+for i in range (7):
     my_list.append(random.randint (3, 10))
 print(my_list)
 new_list_index1 = my_list[-1]
 new_list_index2 = my_list[-3]
 new_list_index3 = my_list[-2]
-print(f"{new_list_index1},{new_list_index2},{new_list_index3}")
+new_list = [new_list_index1, new_list_index2, new_list_index3]
+print(new_list)
