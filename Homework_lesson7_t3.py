74a4faedf5432e2e04f26abc4f4c619c2e33afef
Iryna Korolko <irina.piatenko@gmail.com>
2025-12-30 12:02:11 +0200

    com2

--- Homework_lesson7_t3.py
+++ Homework_lesson7_t3.py
@@ -1,11 +1,7 @@
-def second_index (text, some_str):
+def second_index(text, some_str):
     first_index = text.find(some_str)
     if first_index == -1:
         return None
     else:
-        second_index = text.find (some_str, first_index + len(some_str))
+        second_index = text.find(some_str, first_index + len(some_str))
     return second_index
-
-
-
-
