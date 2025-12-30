1588af448781b06755e3689bcb0656e180d0f9b1
Iryna Korolko <irina.piatenko@gmail.com>
2025-12-30 18:31:16 +0200

    check

--- Homework_lesson7_t4.py
+++ Homework_lesson7_t4.py
@@ -1,5 +1,3 @@
 def common_elements():
-    list1 = [i for i in range(1, 101) if i % 3 == 0]
-    list2 = [i for i in range(1, 101) if i % 5 == 0]
-    common = set(list1) & set(list2)
+    common = set([i for i in range(1, 101) if i % 3 == 0]) & set([i for i in range(1, 101) if i % 5 == 0])
     return common
