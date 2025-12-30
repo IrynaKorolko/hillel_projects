5e2104ff507694f965acec2ca5e09bc3357d867f
Iryna Korolko <irina.piatenko@gmail.com>
2025-12-30 22:48:13 +0200

    com 4

--- Homework_lesson7_t4.py
+++ Homework_lesson7_t4.py
@@ -1,3 +1,2 @@
 def common_elements():
-    common = set([i for i in range(1, 101) if i % 3 == 0]) & set([i for i in range(1, 101) if i % 5 == 0])
-    return common
+    return set([i for i in range(1, 101) if i % 3 == 0]) & set([i for i in range(1, 101) if i % 5 == 0])
