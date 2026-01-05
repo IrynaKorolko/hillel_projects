2022bd3359ee28f0ee028de95194e350ae9cc05b
Iryna Korolko <irina.piatenko@gmail.com>
2026-01-05 18:08:00 +0200

    added Docstring

--- Homework_lesson9_t1.py
+++ Homework_lesson9_t1.py
@@ -1,4 +1,12 @@
 def popular_words(text: str, words: list) -> dict:
+    """The function counts hpw many times the "word" is found in the given text.
+
+    Args:
+        text (str): The text in which to count the words.
+        words (list): A list of words to be counted in the text.
+    Returns:
+        dict: A dictionary with words(as keys) and their values (as counts).
+    """
     our_dictionary = {}
     text_lower = text.lower()
     words_in_text = text_lower.split()
