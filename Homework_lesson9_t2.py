e0cfd8e22099fb5bf9c39485a50bb6793814aa89
Iryna Korolko <irina.piatenko@gmail.com>
2026-01-05 18:11:06 +0200

    Docstring added

--- Homework_lesson9_t2.py
+++ Homework_lesson9_t2.py
@@ -1,2 +1,8 @@
-def difference(*args):
+def difference(*args: float, int):
+    """The function returns the difference between the maximum and minimum values of arguments.
+    Args:
+        *args: values (any number).
+    Returns:
+        The difference between the maximum and minimum values.
+    """
     return 0 if len(args) == 0 else max(args) - min(args)
