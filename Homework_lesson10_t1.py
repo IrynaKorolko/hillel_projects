44b87e86fe36a48ffbba7cfab1e7adb8f8983753
Iryna Korolko <irina.piatenko@gmail.com>
2026-01-06 17:34:47 +0200

    com2

--- Homework_lesson10_t1.py
+++ Homework_lesson10_t1.py
@@ -5,4 +5,4 @@ def is_even(digit) -> bool:
     Returns:
         bool: True if the number is even, False if not.
     """
-    return True if digit %2 ==0 else False
+    return True if digit %2 == 0 else False
