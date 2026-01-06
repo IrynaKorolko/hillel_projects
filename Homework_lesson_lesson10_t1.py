90ccab6721a7c04a229963d8d33d97380417475d
Iryna Korolko <irina.piatenko@gmail.com>
2026-01-06 17:25:44 +0200

    coom1

---
+++ Homework_lesson_lesson10_t1.py
@@ -0,0 +1,8 @@
+def is_even(digit) -> bool:
+    """The function checks if the number is even.
+    Args:
+        digit: int number
+    Returns:
+        bool: True if the number is even, False if not.
+    """
+    return True if digit %2 ==0 else False
