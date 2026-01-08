4e3e4a97cce5de7e10192e46c1b34edf7ed5c065
Iryna Korolko <irina.piatenko@gmail.com>
2026-01-08 22:57:08 +0200

    com2

--- Homework_lesson10_t1.py
+++ Homework_lesson10_t1.py
@@ -1,6 +1,21 @@
-def pow(x):
-    return x**2
-def some_gen(begin, n, func):
+def pow(x) -> float:
+    """The function returns the square of x
+
+    Parameters: A number to be squared.
+
+    Returns: The square of x.
+    """
+    return x ** 2
+
+def some_gen(begin, n, func) -> object:
+    """The generator yields n elements, starting from begin to get the next element.
+    Parameters:
+        begin: The starting element.
+        n: The number of elements.
+        func: A function that takes one argument and returns the next element.
+    Yields:
+        The next element in the sequence.
+    """
     count = 0
     start_el = begin
     while count < n:
