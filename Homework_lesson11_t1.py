3d98cbb8ab1908bf9cc741b0d183bcf968a4f6e2
Iryna Korolko <irina.piatenko@gmail.com>
2026-01-13 17:42:08 +0200

    com1

---
+++ Homework_lesson11_t1.py
@@ -0,0 +1,20 @@
+def prime_generator(end):
+    """Generate prime numbers up to a limit set.
+
+    Args:
+        end (int): The upper limit.
+
+    Yields:
+        int: The next prime number in the list.
+    """
+    for num in range(2, end):
+        is_prime = True
+        for i in range(2, num):
+            if num % i == 0:
+                is_prime = False
+                break
+        if is_prime:
+            yield 
+            
+
+
