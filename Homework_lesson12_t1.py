314d2b11171b8d5f2c04e0ef1057453adb935f1c
Iryna Korolko <irina.piatenko@gmail.com>
2026-01-14 23:48:58 +0200

    commit 1

---
+++ Homework_lesson12_t1.py
@@ -0,0 +1,20 @@
+def delete_html_tags(html_file = "draft.html", result_file = "cleaned.txt"):
+    """
+    This function reads an HTML file, removes all HTML tags, and writes the cleaned text to a new file.
+    
+    Parameters: html_file
+
+    Result: File to the output file where cleaned text will be saved.
+    """
+    import re
+
+    
+    with open(html_file, 'r', encoding='utf-8') as file:
+        file_content = file.read()
+        cleaned_text = re.sub(r'<[^>]+>', '', file_content)
+    with open(result_file, 'w', encoding='utf-8') as result:
+        result.write(cleaned_text)
+        
+
+delete_html_tags()
+
